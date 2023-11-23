218dcab6e857fc341587e1e3548153e46fe942d0
Sylvain Baudouin <sylvain.baudouin273@gmail.com>
2023-11-23 20:46:06 +0100

    init data

---
+++ main.py
@@ -0,0 +1,12 @@
+import json
+
+file_path = "data/goodreads_reviews_dedup.json"
+dest_path = "data/test.json"
+lines_number = 90000
+
+with open(file_path) as input_file:
+    head = [next(input_file) for _ in range(lines_number)]
+
+with open(dest_path, 'w') as output_file:
+    output_file.writelines('['+','.join(head)+']')
+        
