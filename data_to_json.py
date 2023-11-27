163f37e3a3dfe826c9ea7ca8a8786c91c87f53c7
Mathieu <creepymat44@gmail.com>
2023-11-27 16:58:24 +0100

    ca avance

--- data_to_json.py
+++ data_to_json.py
@@ -1,6 +1,6 @@
 file_path = "data/goodreads_reviews_dedup.json"
 dest_path = "data/reviews.json"
-lines_number = 90000
+lines_number = 30000
 
 with open(file_path) as input_file:
     head = [next(input_file) for _ in range(lines_number)]
